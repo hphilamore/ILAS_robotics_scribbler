5c6b2dab20ce22f4fa7f988388298c327c926161
hphilamore <hemmaphilamore@gmail.com>
2018-11-09 22:24:05 +0900

    commented out code removed to tidy

--- BinaryProportionalLineFollow/BinaryProportionalLineFollow.c
+++ BinaryProportionalLineFollow/BinaryProportionalLineFollow.c
@@ -71,36 +71,7 @@ int main()
       // Calibrate IR line sensors for WHITE
       // Turn on spot 
       WhiteCalibrate();
-  
-      /*
-      encoder_update();                          // get new encoder values
-      float left_count_start = encoder_vals[0];  // left encoder count at start 
-      
-      int left = 0;                              // variables for IR sensor values      
-      int right = 0;
-      int count = 0;
-      
-      int bot_diameter = 145;                    // distance between two drive wheels
-      
-      // turn until full circle reached
-      while(fabs(encoder_vals[0] - left_count_start) < bot_diameter * 3.142){     
-        
-        s3_motorSet(50, -50, 0);                 // turn on spot
-        left += s3_lineSensor(S3_LEFT);          // cumulative sum left IR sensor
-        right += s3_lineSensor(S3_RIGHT);        // cumulative sum right IR sensor
-        count += 1;                              // cumulative sum while loops
-        encoder_update();                        // get new encoder values
-      }
-      s3_motorSet(0, 0, 0);                  // stop moving         
-      
-      int leftave_white = left/count;        // average left IR sensor
-      int rightave_white = right/count;      // average right IR sensor
-      
-      s3_memoryWrite(1, leftave_white);      // store in non-volatile memory 
-      s3_memoryWrite(2, rightave_white);
-      */
-
-      
+       
     }  
     
     
@@ -108,112 +79,20 @@ int main()
       // Calibrate IR line sensors for BLACK
       // Drive 5cm in straight line 
       
-      BlackCalibrate();
-      /*
-      
-      encoder_update();                          // get new encoder values
-      float left_count_start = encoder_vals[0];  // left encoder count at start 
-      
-      int left = 0;                              // variables for IR sensor values      
-      int right = 0;
-      int count = 0;
-      
-      int bot_diameter = 145;                    // distance between two drive wheels
-      
-      // turn until 5cm linear travel reached
-      while(fabs(encoder_vals[0] - left_count_start) < 50){     
-        
-        s3_motorSet(50, 50, 0);                 // turn on spot
-        left += s3_lineSensor(S3_LEFT);         // cumulative sum left IR sensor
-        right += s3_lineSensor(S3_RIGHT);       // cumulative sum right IR sensor
-        count += 1;                             // cumulative sum while loops
-        encoder_update();                       // get new encoder values
-      }
-      s3_motorSet(0, 0, 0);                  // stop moving         
-      
-      int leftave_black = left/count;        // average left IR sensor
-      int rightave_black = right/count;      // average right IR sensor
-      
-      s3_memoryWrite(3, leftave_black);      // store in non-volatile memory 
-      s3_memoryWrite(4, rightave_black); 
-      */
-      
+      BlackCalibrate();      
       
     }     
     
     if (s3_resetButtonCount() == 3) {
       // line following : binary controller
-      LineFollowBinary();
-      
-      /*
-      int leftave_white = s3_memoryRead(1);                      // stored average IR sensor calibration values
-      int rightave_white = s3_memoryRead(2);
-      int leftave_black = s3_memoryRead(3);
-      int rightave_black = s3_memoryRead(4);
-      
-      int mid_left = (leftave_white - leftave_black) / 2;        // midpoint between black and white
-      int mid_right = (rightave_white - rightave_black) / 2;
-      
-      int v_basic = 30;                                          // basic speed
-      
-      while(1){
-      
-      if(s3_lineSensor(S3_LEFT) > mid_left && s3_lineSensor(S3_RIGHT) < mid_right){       // left=white, right=black --> turn right
-        s3_motorSet(40, -20, 0);
-        pause(200);}
-      
-      else if(s3_lineSensor(S3_RIGHT) > mid_right && s3_lineSensor(S3_LEFT) < mid_left){  // left=black, right=white --> turn left
-        s3_motorSet(-20, 40, 0);
-        pause(200);}
-       
-      else{                              // left = right = white/black --> go straight
-        s3_motorSet(v_basic, v_basic, 0);     
-        pause(200);}
-      
-      } 
-      */
-
-      
+      LineFollowBinary();    
       
     } 
         
         
      if (s3_resetButtonCount() == 4) {
       // line following : proportional controller
-      LineFollowProp();
-      
-      /*
-      int leftave_white = s3_memoryRead(1);                      // stored average IR sensor calibration values
-      int rightave_white = s3_memoryRead(2);
-      int leftave_black = s3_memoryRead(3);
-      int rightave_black = s3_memoryRead(4);
-      
-      int mid_left = (leftave_white - leftave_black) / 2;        // midpoint between black and white
-      int mid_right = (rightave_white - rightave_black) / 2;
-      
-      int v_basic = 30;                                          // basic speed
-
-      
-      while(1){
-      
-        if(s3_lineSensor(S3_LEFT) > mid_left && s3_lineSensor(S3_RIGHT) < mid_right){          // left=white, right=black --> turn right
-          int r_error = rightave_white - s3_lineSensor(S3_RIGHT);
-          int v_l = v_basic + (100 - v_basic) * r_error / (rightave_white - rightave_black);
-          s3_motorSet(v_l, -v_l/2, 0);
-          pause(200);}
-        else if(s3_lineSensor(S3_RIGHT) > mid_right && s3_lineSensor(S3_LEFT) < mid_left){     // left=black, right=white --> turn left
-          int l_error = leftave_white - s3_lineSensor(S3_LEFT);
-          int v_r = v_basic + (100 - v_basic) * l_error / (leftave_white - leftave_black);
-          s3_motorSet(-v_r/2, v_r, 0);
-          pause(200);}
-        else{                                     // left = right = white/black --> go straight
-          s3_motorSet(v_basic, v_basic, 0);
-          pause(200);}
-
-      
-      }
-      */ 
-            
+      LineFollowProp();           
           
     }   
    
