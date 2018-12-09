e307407a38a286e39ed76aa0bec9a1deba217b9a
hphilamore <hemmaphilamore@gmail.com>
2018-12-09 09:53:21 +0900

    in encoder test, encoder count streamlined down. counter on seperate cog moved outside of button press so that it can be applied in all programs

--- ILAS_encoder_test/ILAS_encoder_test.c
+++ ILAS_encoder_test/ILAS_encoder_test.c
@@ -20,12 +20,14 @@ unsigned int stack[40 + 25];                  // Stack vars for other cog
 int main()                                    // main function
 {
   s3_setup();
-  t = 50;                                     // Set values of t & n
-  n = 5000;
-
+  //t = 50;                                     // Set values of t & n
+  //n = 5000;
+  
+  cogstart(encoder_update, NULL, stack, sizeof(stack));
+  
   if (s3_resetButtonCount() == 1) {
   // Launch encoder_update function into another cog (processor).
-  cogstart(encoder_update, NULL, stack, sizeof(stack));
+  //cogstart(encoder_update, NULL, stack, sizeof(stack));
   //cog_run(encoder_update, 65);
 
   // Watch what the other cog is doing to the value of n.
@@ -38,12 +40,48 @@ int main()                                    // main function
     //print("%f \n", encoder_vals[0]); 
     //GoToGoal();
     //GoToGoal(200, 100, pi/4, 50); 
-    GoToGoal(200, -100, pi/4, 50);   
+    GoToGoal(200, 100, pi/4, 50);   
     //GoToGoal(200, -100, pi/4, 50);   
     //GoToGoal(-200, -100, pi/4, 50);     
     //cog_run(encoder_update, 128);                  // Run blink in other cog
   //} 
-}     
+  }
+  
+  /*
+  if (s3_resetButtonCount() == 2) {
+    
+    //cogstart(encoder_update, NULL, stack, sizeof(stack));
+    
+    while(1){
+      s3_motorSet( 50, 50, 0);   
+      int32_t value = scribbler_motion();
+      print("%d  \t", (value >> 24)); 
+      //print("%f  \n", encoder_vals[0]);  
+   }     
+  
+  } 
+  */
+  
+  /*
+    
+  if (s3_resetButtonCount() == 3) {
+    s3_motorSet( 50, 50, 0);  
+    cogstart(encoder_update, NULL, stack, sizeof(stack));
+    
+    print("%f  \n", encoder_vals[0]);  
+    while(1){
+      //s3_motorSet( 50, 50, 0);   
+      int32_t value = scribbler_motion();
+      print("%d  \t", (value >> 24)); 
+      //print("%f  \n", encoder_vals[0]); 
+      print("%f  \n", encoder_vals[0]);   
+   }     
+  
+  } 
+  */  
+
+
+     
 }
 
 
@@ -61,8 +99,8 @@ void GoToGoal(float dxI, float dyI, float theta, int basic_speed){
   // convert local x,y to polar coordinates                        
   float aR = angle2pi(dxR, dyR);                             // angle
   float dR = powf((powf(dxR,2) + powf(dyR,2)), 0.5);         // distance
-  print("%f\n", dR);
-  print("%f\n", dR);
+  //print("%f\n", dR);
+  //print("%f\n", dR);
   
   
   
@@ -76,7 +114,7 @@ void GoToGoal(float dxI, float dyI, float theta, int basic_speed){
   
   if(turn_flag == 0 || straight_flag == 0){
     
-    //print("\t %f \t", encoder_vals[0]); 
+    //print("%f \t", encoder_vals[0]); 
     //print("left encoder %f, start count %f, lenR %f, dR %f \n",   encoder_vals[0], left_count_start, lenR, dR);
     //print("start count %f \t", left_count_start);
     //print("lenR %f \n", lenR);
@@ -101,7 +139,7 @@ void GoToGoal(float dxI, float dyI, float theta, int basic_speed){
      if(fabs(encoder_vals[0] - left_count_start) >= fabs(dR)){
           // put the flag up
           straight_flag = 1;
-          print("reached!");
+          //print("reached!");
           s3_simpleStop();
           //s3_motorSet( 0, 0, 0);  
         }                  
@@ -120,7 +158,7 @@ void GoToGoal(float dxI, float dyI, float theta, int basic_speed){
 
 
 
-
+/*
 void encoder_update(void) { 
     while(1){  
     // Value : a 32 bit integer containing registers describig the behavious of drive and idler wheel encoders
@@ -171,6 +209,124 @@ void encoder_update(void) {
   }                 
 
 }
+
+*/
+
+/*
+void encoder_update(void) { // working version from 18_12_08
+    while(1){  
+    // Value : a 32 bit integer containing registers describig the behavious of drive and idler wheel encoders
+    // Updates an array of 9 values
+    // 0 Left wheel distance count (mm)
+    // 1 Right wheel distance count (mm)
+    // 2 Time in 1/10 second since the last idler encoder edge
+    // 3 Idler wheel velocity
+    // 4 Non-zero if one or more motors are turning.  
+    // 5-8 Variables used in encoder calcs     
+
+    
+    int32_t value = scribbler_motion();
+    
+    int32_t e0 = (value >> 24);           
+    encoder_vals[7] = e0 * 0.25;  //new 1           
+    
+    int32_t e1 = (value >> 16) & 0xff;           
+    encoder_vals[8] = e1 * 0.25;  //new 2  
+      
+      //if (new > old){
+      if (encoder_vals[7] >= encoder_vals[5]){
+        encoder_vals[0] += encoder_vals[7] - encoder_vals[5];
+      }
+      else {  
+        encoder_vals[0] += encoder_vals[7]; 
+      }    
+      
+      if (encoder_vals[8] >= encoder_vals[6]){
+        encoder_vals[1] += encoder_vals[8]-encoder_vals[6];
+      }
+      else {  
+        encoder_vals[1] += encoder_vals[8]; 
+      }             
+        
+    //old = new;
+    encoder_vals[5] = encoder_vals[7];  // old1 = new1
+    encoder_vals[6] = encoder_vals[8];  // old2 = new2
+    
+    int32_t e2 = (value >> 8)  & 0xff;           
+    encoder_vals[2] = e2;
+    
+    int32_t e3 = value & 0xfc;           
+    encoder_vals[3] = e3;
+    
+    int32_t e4 = value & 0x3;           
+    encoder_vals[4] = e4; 
+    
+    //print("%d  \t", (value >> 24)); 
+    //print("%f  \n", encoder_vals[0]);  
+  }                 
+
+}
+*/
+
+
+void encoder_update(void) { 
+    while(1){  
+    // Value : a 32 bit integer containing registers describig the behavious of drive and idler wheel encoders
+    // Updates an array of 9 values
+    // 0 Left wheel distance count (mm)
+    // 1 Right wheel distance count (mm)
+    // 2 Time in 1/10 second since the last idler encoder edge
+    // 3 Idler wheel velocity
+    // 4 Non-zero if one or more motors are turning.  
+    // 5-8 Variables used in encoder calcs     
+
+    
+    int32_t value = scribbler_motion();
+    
+    //int32_t e0 = (value >> 24);           
+    //encoder_vals[7] = e0 * 0.25;  //new 1 
+    encoder_vals[7] = (value >> 24) * 0.25;  //new 1           
+    
+    //int32_t e1 = (value >> 16) & 0xff;           
+    //encoder_vals[8] = e1 * 0.25;  //new 2 
+    encoder_vals[8] = ((value >> 16) & 0xff) * 0.25;  //new 2  
+      
+      //if (new > old){
+      //if (encoder_vals[7] >= encoder_vals[5]){
+      if (abs(encoder_vals[7]) >= abs(encoder_vals[5])){
+        encoder_vals[0] += encoder_vals[7] - encoder_vals[5];
+      }
+      else {  
+        encoder_vals[0] += encoder_vals[7]; 
+      }    
+      
+      //if (encoder_vals[8] >= encoder_vals[6]){
+      if (abs(encoder_vals[8]) >= abs(encoder_vals[6])){
+        encoder_vals[1] += encoder_vals[8]-encoder_vals[6];
+      }
+      else {  
+        encoder_vals[1] += encoder_vals[8]; 
+      }             
+        
+    //old = new;
+    encoder_vals[5] = encoder_vals[7];  // old1 = new1
+    encoder_vals[6] = encoder_vals[8];  // old2 = new2
+    
+    //int32_t e2 = (value >> 8)  & 0xff;           
+    encoder_vals[2] = (value >> 8)  & 0xff;
+    
+    //int32_t e3 = value & 0xfc;           
+    encoder_vals[3] = value & 0xfc;
+    
+    //int32_t e4 = value & 0x3;           
+    encoder_vals[4] = value & 0x3; 
+    
+    //print("%d  \t", (value >> 24)); 
+    //print("%f  \n", encoder_vals[0]);  
+  }                 
+
+}
+
 
 
 float angle2pi(float x, float y){
